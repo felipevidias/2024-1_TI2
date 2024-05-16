d3c9a43b5d664749c2d1f1bad3eeb4dd4f5ecda3
Felipe Vilhena Dias <121449886+felipevidias@users.noreply.github.com>
2024-05-16 18:28:23 -0300

    Add files via upload

--- site_t1/codigo sem videos/scripthome.js
+++ site_t1/codigo sem videos/scripthome.js
@@ -4,22 +4,31 @@ const sign = document.querySelector('#sign');
 const videoaulas = document.querySelector('#vid')
 const perfil = document.querySelector('#perfil')
 const adicionar = document.querySelector('#adicionar')
+const nav = document.querySelector('.navigation')
+
+if (localStorage.getItem("userLogado")) {
+  nav.removeChild(log.parentElement);
+  nav.removeChild(sign.parentElement);
+}
+else {
+  nav.removeChild(perfil.parentElement);
+}
 
 
 
-log.addEventListener('click', function() {
+log.addEventListener('click', function () {
   window.location.href = 'forms.html';
 });
 
-sign.addEventListener('click', function() {
-    window.location.href = 'forms.html';
+sign.addEventListener('click', function () {
+  window.location.href = 'forms.html';
 });
 
-videoaulas.addEventListener('click', function() {
+videoaulas.addEventListener('click', function () {
   window.location.href = 'indexvideoaulas.html';
 });
 
-perfil.addEventListener('click', function() {
+perfil.addEventListener('click', function () {
   window.location.href = 'indexperfil.html';
 });
 
@@ -30,59 +39,63 @@ const di3 = document.querySelector('#di3');
 const co1 = document.querySelector('#co1');
 const co2 = document.querySelector('#co2');
 const co3 = document.querySelector('#co3');
-const x1 = document.querySelector('#x1');
-const x2 = document.querySelector('#x2');
-const x3 = document.querySelector('#x3');
+const ol = document.querySelector('.overlay');
 
 // Oculta as divs de id "co(numero)"
 co1.style.display = 'none';
 co2.style.display = 'none';
 co3.style.display = 'none';
+ol.style.display = 'none';
+co1.style.visibility = 'hidden';
+co2.style.visibility = 'hidden';
+co3.style.visibility = 'hidden';
 
 // Adiciona os event listeners para as divs di1, di2 e di3
-di1.addEventListener('click', function() {
+di1.addEventListener('click', function () {
+  ol.style.display = 'inherit';
   co1.style.display = 'flex';
+  co1.style.visibility = 'visible';
   co2.style.display = 'none';
+  co2.style.visibility = 'hidden';
   co3.style.display = 'none';
+  co3.style.visibility = 'hidden';
 });
 
-di2.addEventListener('click', function() {
+di2.addEventListener('click', function () {
+  ol.style.display = 'inherit';
   co1.style.display = 'none';
+  co1.style.visibility = 'hidden';
   co2.style.display = 'flex';
+  co2.style.visibility = 'visible';
   co3.style.display = 'none';
+  co3.style.visibility = 'hidden';
 });
 
-di3.addEventListener('click', function() {
+di3.addEventListener('click', function () {
+  ol.style.display = 'inherit';
   co1.style.display = 'none';
+  co1.style.visibility = 'hidden';
   co2.style.display = 'none';
+  co2.style.visibility = 'hidden';
   co3.style.display = 'flex';
+  co3.style.visibility = 'visible';
 });
 
-x1.addEventListener('click', function() {
+ol.addEventListener('click', function (evt) {
+  if (!(evt.target === ol)) {
+    return;
+  }
   co1.style.display = 'none';
+  co1.style.visibility = 'hidden';
+  co2.style.visibility = 'hidden';
+  co3.style.visibility = 'hidden';
   co2.style.display = 'none';
   co3.style.display = 'none';
-});
-
-x2.addEventListener('click', function() {
-  co1.style.display = 'none';
-  co2.style.display = 'none';
-  co3.style.display = 'none';
-});
-
-x3.addEventListener('click', function() {
-  co1.style.display = 'none';
-  co2.style.display = 'none';
-  co3.style.display = 'none';
+  ol.style.display = 'none';
 });
 
 
 
 //Mensagem estilizada no console
-console.log("                                             ");
-console.log(" _____ _       _   _         _     _ _       ");
-console.log("| __  | |_ _ _| |_| |_ _____|_|___|_| |_ _ _ ");
-console.log("|    -|   | | |  _|   |     | |  _| |  _| | |");
-console.log("|__|__|_|_|_  |_| |_|_|_|_|_|_|___|_|_| |_  |");
-console.log("          |___|                         |___|");
-console.log("                                             ");
+console.log("\n╭━━━╮╱╱╭╮╱╱╭╮╱╱╱╱╱╭━━╮╱╱╱╱╱╱╭╮ \n╰╮╭╮┃╱╱┃┃╱╭╯╰╮╱╱╱╱┃╭╮┃╱╱╱╱╱╭╯╰╮\n╱┃┃┃┣┳━╯┣━┻╮╭╋┳━━╮┃╰╯╰┳━━┳━┻╮╭╯\n╱┃┃┃┣┫╭╮┃╭╮┃┃┣┫╭━╯┃╭━╮┃┃━┫╭╮┃┃ \n╭╯╰╯┃┃╰╯┃╭╮┃╰┫┃╰━╮┃╰━╯┃┃━┫╭╮┃╰╮\n╰━━━┻┻━━┻╯╰┻━┻┻━━╯╰━━━┻━━┻╯╰┻━╯\n                               ");
+
